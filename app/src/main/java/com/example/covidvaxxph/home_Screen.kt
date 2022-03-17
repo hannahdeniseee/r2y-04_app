fcc0bd68b9cfc2118c82e3f6f3a36b5598467509
hannahdeniseee <hannahdenisebacomo@gmail.com>
2022-03-17 16:31:13 +0800

    Covid-19 Vaxx PH App - R2Y-04

--- app/src/main/java/com/example/covidvaxxph/home_Screen.kt
+++ app/src/main/java/com/example/covidvaxxph/home_Screen.kt
@@ -19,10 +19,17 @@ class home_Screen : AppCompatActivity() {
 
         val imageButton2 = findViewById<ImageButton>(R.id.imageButton2)
         imageButton2.setOnClickListener{
-            val intent = Intent(this, user_guide::class.java)
+            val intent = Intent(this, vaxx_info::class.java)
             startActivity(intent)
         }
 
+        val imageButton3 = findViewById<ImageButton>(R.id.imageButton3)
+        imageButton3.setOnClickListener{
+            val intent = Intent(this, vaxx_ph::class.java)
+            startActivity(intent)
+        }
+        
+            
     }
 
 
---
+++ app/src/main/java/com/example/covidvaxxph/vaxx_ph.kt
@@ -0,0 +1,12 @@
+package com.example.covidvaxxph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class vaxx_ph : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vaxx_ph)
+        //getSupportActionBar.setTitle("Vaccines in the Philippines") <-- not working :(
+    }
+}
