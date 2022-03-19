a0871712f23c2edbaca6a1442f582109ced7dbd0
hannahdeniseee <hannahdenisebacomo@gmail.com>
2022-03-19 13:32:26 +0800

    Covid-19 Vaxx PH App - R2Y-04

--- app/src/main/java/com/example/covidvaxxph/vaxx_info.kt
+++ app/src/main/java/com/example/covidvaxxph/vaxx_info.kt
@@ -2,10 +2,14 @@ package com.example.covidvaxxph
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class vaxx_info : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vaxx_info)
+
+
     }
 }
