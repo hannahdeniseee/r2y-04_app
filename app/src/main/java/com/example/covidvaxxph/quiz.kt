1bc16111e9acd1c30d17f4f26e4057ff55417d44
wonyochi <b2023mjtoribio@pshs.edu.ph>
2022-03-29 22:47:47 +0800

    [Quiz] quiz is working (selected option, correct and incorrect answers are highlighted, submit button works as well and goes to the next question) pagod na ako gusto ko na mag-kdrama

--- app/src/main/java/com/example/covidvaxxph/quiz.kt
+++ app/src/main/java/com/example/covidvaxxph/quiz.kt
@@ -1,6 +1,8 @@
 package com.example.covidvaxxph
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,37 +13,84 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
-class quiz : AppCompatActivity() {
+class quiz : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
-        val questionsList = Constants.getQuestions()
-        Log.i("Questions Size", "${questionsList.size}")
+        mQuestionsList = Constants.getQuestions()
 
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition-1]
+        setQuestion()
 
-        var progressBar = findViewById<ProgressBar>(R.id.progress_bar) as ProgressBar
-        progressBar.progress = currentPosition
-        var progressText = findViewById<TextView>(R.id.progress_text)
-        progressText.text = "$currentPosition" + "/" + progressBar.max
-        var questionText = findViewById<TextView>(R.id.question_text)
-        questionText.text = question!!.question
-
-        var option1Text = findViewById<TextView>(R.id.quiz_option1)
-        option1Text.text = question.option1
-        var option2Text = findViewById<TextView>(R.id.quiz_option2)
-        option2Text.text = question.option2
-        var option3Text = findViewById<TextView>(R.id.quiz_option3)
-        option3Text.text = question.option3
-        var option4Text = findViewById<TextView>(R.id.quiz_option4)
-        option4Text.text = question.option4
+        findViewById<TextView>(R.id.quiz_option1).setOnClickListener(this)
+        findViewById<TextView>(R.id.quiz_option2).setOnClickListener(this)
+        findViewById<TextView>(R.id.quiz_option3).setOnClickListener(this)
+        findViewById<TextView>(R.id.quiz_option4).setOnClickListener(this)
+        // submit
+        findViewById<TextView>(R.id.quiz_submitbutton).setOnClickListener(this)
 
         //Toolbar toolbar = findViewById(R.id.toolbar)
         val mtoolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(mtoolbar)
 
+    }
+
+    private fun setQuestion() {
+
+        val question = mQuestionsList!![mCurrentPosition-1]
+
+        defaultOptionsView()
+
+        if(mCurrentPosition == mQuestionsList!!.size){
+            findViewById<TextView>(R.id.quiz_submitbutton).text = "FINISH"
+        }
+        else {
+            findViewById<TextView>(R.id.quiz_submitbutton).text = "SUBMIT"
+        }
+
+        // var progressBar here
+        var progressBar = findViewById<ProgressBar>(R.id.progress_bar) as ProgressBar
+        progressBar.progress = mCurrentPosition
+        // var progressText here
+        var progressText = findViewById<TextView>(R.id.progress_text)
+        progressText.text = "$mCurrentPosition" + "/" + progressBar.max
+        // var questionText here
+        var questionText = findViewById<TextView>(R.id.question_text)
+        questionText.text = question!!.question
+
+        // var option1Text here
+        var option1Text = findViewById<TextView>(R.id.quiz_option1)
+        option1Text.text = question.option1
+        // var option2Text here
+        var option2Text = findViewById<TextView>(R.id.quiz_option2)
+        option2Text.text = question.option2
+        // var option3Text here
+        var option3Text = findViewById<TextView>(R.id.quiz_option3)
+        option3Text.text = question.option3
+        // var option4Text here
+        var option4Text = findViewById<TextView>(R.id.quiz_option4)
+        option4Text.text = question.option4
+    }
+
+    private fun defaultOptionsView() {
+        val options = ArrayList<TextView>()
+        options.add(0, findViewById<TextView>(R.id.quiz_option1))
+        options.add(1, findViewById<TextView>(R.id.quiz_option2))
+        options.add(2, findViewById<TextView>(R.id.quiz_option3))
+        options.add(3, findViewById<TextView>(R.id.quiz_option4))
+
+        for (option in options){
+            option.setTextColor(Color.parseColor("#7A8089")) // if not selected
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_bg)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -79,5 +128,77 @@ class quiz : AppCompatActivity() {
             startActivity(intent)
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.quiz_option1 ->{
+                selectedOptionView(findViewById<TextView>(R.id.quiz_option1), 1)
+            }
+            R.id.quiz_option2 ->{
+                selectedOptionView(findViewById<TextView>(R.id.quiz_option2), 2)
+            }
+            R.id.quiz_option3 ->{
+                selectedOptionView(findViewById<TextView>(R.id.quiz_option3), 3)
+            }
+            R.id.quiz_option4 ->{
+                selectedOptionView(findViewById<TextView>(R.id.quiz_option4), 4)
+            }
+            R.id.quiz_submitbutton ->{
+                if(mSelectedOptionPosition == 0){
+                    mCurrentPosition++
+                    when{
+                        mCurrentPosition <= mQuestionsList!!.size ->{
+                            setQuestion()
+                        }
+                        else ->{
+                            Toast.makeText(this, "Quiz done!", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                else {
+                    val question = mQuestionsList?.get(mCurrentPosition-1)
+                    if (question!!.answer != mSelectedOptionPosition){
+                        answerView(mSelectedOptionPosition, R.drawable.incorrect_option_bg)
+                    }
+                    answerView(question.answer, R.drawable.correct_option_bg)
+
+                    if (mCurrentPosition == mQuestionsList!!.size){
+                        findViewById<TextView>(R.id.quiz_submitbutton).text = "FINISH"
+                    }
+                    else {
+                        findViewById<TextView>(R.id.quiz_submitbutton).text = "GO TO NEXT QUESTION"
+                    }
+                    mSelectedOptionPosition = 0
+                }
+            }
+        }
+    }
+
+    private fun answerView(answer: Int, drawableView: Int){
+        when(answer){
+            1 ->{
+                findViewById<TextView>(R.id.quiz_option1).background = ContextCompat.getDrawable(this, drawableView)
+            }
+            2 ->{
+                findViewById<TextView>(R.id.quiz_option2).background = ContextCompat.getDrawable(this, drawableView)
+            }
+            3 ->{
+                findViewById<TextView>(R.id.quiz_option3).background = ContextCompat.getDrawable(this, drawableView)
+            }
+            4 ->{
+                findViewById<TextView>(R.id.quiz_option4).background = ContextCompat.getDrawable(this, drawableView)
+            }
+        }
+    }
+
+    // selected option highlight
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int){
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(Color.parseColor("#363A43")) // if selected
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(this, R.drawable.selected_option_bg)
     }
 }
