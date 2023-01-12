0400fd2e58b8b181fce75123bdaa7348c97b1a42
wonyochi <b2023mjtoribio@pshs.edu.ph>
2023-01-12 10:09:50 +0800

    [ENG Vaxx Guide] Added menu bar

--- app/src/main/java/com/example/covidvaxxph/vaxx_reg.kt
+++ app/src/main/java/com/example/covidvaxxph/vaxx_reg.kt
@@ -1,13 +1,21 @@
 package com.example.covidvaxxph
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class vaxx_reg : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vaxx_reg)
+        val mtoolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(mtoolbar)
 
         //A Window
         val reg_stepbtnA = findViewById<View>(R.id.reg_stepbtnA)
@@ -33,12 +41,49 @@ class vaxx_reg : AppCompatActivity() {
             expanded.show(supportFragmentManager, "customDialog")
         }
 
-        //C Window
+        //DOH Window
         val reg_stepbtnDOH = findViewById<View>(R.id.reg_stepbtnDOH)
         reg_stepbtnDOH.setOnClickListener{
             var expanded = sputnik_expanded()
 
             expanded.show(supportFragmentManager, "customDialog")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_bar,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item?.itemId
+        if (id == R.id.home_navbutton) {
+            Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, home_Screen::class.java)
+            startActivity(intent)
+        }
+
+        if (id == R.id.inter_navbutton) {
+            Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, interactive_page::class.java)
+            startActivity(intent)
+        }
+        if (id == R.id.ph_navbutton) {
+            Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, vaxx_ph::class.java)
+            startActivity(intent)
+        }
+        if (id == R.id.user_navbutton){
+            Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, user_guide::class.java)
+            startActivity(intent)
+        }
+
+        if (id == R.id.info_navbutton){
+            Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, vaxx_info::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
