9425b712742e4e0bad8cbc61044415a26a735428
Mary Grace Larisca <b2023mglarisca@pshs.edu.ph>
2023-01-16 22:55:20 +0800

    vax reg (fil done) *except for doh link

---
+++ app/src/main/java/com/example/covidvaxxph/reg_fil_expanded_b.kt
@@ -0,0 +1,20 @@
+package com.example.covidvaxxph
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class reg_fil_expanded_b: DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var rootView: View = inflater.inflate(R.layout.reg_fil_expanded_b, container, false)
+
+        return rootView
+    }
+}
---
+++ app/src/main/java/com/example/covidvaxxph/vaxx_fil_reg.kt
@@ -0,0 +1,88 @@
+package com.example.covidvaxxph
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+
+class vaxx_fil_reg : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vaxx_reg_fil1)
+        val mtoolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(mtoolbar)
+
+        //A Window
+        val reg_stepbtnA = findViewById<View>(R.id.reg_fil_stepbtnA)
+        reg_stepbtnA.setOnClickListener{
+            var expanded = reg_fil_expanded_a()
+
+            expanded.show(supportFragmentManager, "customDialog")
+        }
+
+        //B Window
+        val reg_stepbtnB = findViewById<View>(R.id.reg_fil_stepbtnB)
+        reg_stepbtnB.setOnClickListener{
+            var expanded = reg_fil_expanded_b()
+
+            expanded.show(supportFragmentManager, "customDialog")
+        }
+
+        //C Window
+        val reg_stepbtnC = findViewById<View>(R.id.reg_fil_stepbtnC)
+        reg_stepbtnC.setOnClickListener{
+            var expanded = reg_fil_expanded_c()
+
+            expanded.show(supportFragmentManager, "customDialog")
+        }
+
+        //DOH Window
+        val reg_stepbtnDOH = findViewById<View>(R.id.reg_fil_stepbtnDOH)
+        reg_stepbtnDOH.setOnClickListener{
+            var expanded = sputnik_expanded()
+
+            expanded.show(supportFragmentManager, "customDialog")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_bar,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item?.itemId
+        if (id == R.id.home_navbutton) {
+            Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, home_Screen::class.java)
+            startActivity(intent)
+        }
+
+        if (id == R.id.inter_navbutton) {
+            Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, interactive_page::class.java)
+            startActivity(intent)
+        }
+        if (id == R.id.ph_navbutton) {
+            Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, vaxx_ph::class.java)
+            startActivity(intent)
+        }
+        if (id == R.id.user_navbutton){
+            Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, user_guide::class.java)
+            startActivity(intent)
+        }
+
+        if (id == R.id.info_navbutton){
+            Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+            val intent = Intent(this, vaxx_info::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
